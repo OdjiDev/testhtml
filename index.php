76f83450589893593f2793e2ca885ec588b21855
Djire <odjidev@gmail.com>
2024-05-08 08:22:05 +0000

    Changement de titre essaie  de MAJ

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login page in HTML</title>
+    <title> Bienvenue a la page de commexion pour la gestion des ressources</title>
 </head>
 <body style="background-color: pink;">
     <h1>Login form using HTML and CSS</h1>
