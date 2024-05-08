a7224f3a08d932ada68b9551e375947e7c5a0e43
Djire <odjidev@gmail.com>
2024-05-08 07:55:28 +0000

    collor  essaie de MAJ

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login page in HTML</title>
 </head>
-<body>
+<body style="background-color: pink;">
     <h1>Login form using HTML and CSS</h1>
     <form action="">
         <!-- Headings for the form -->
